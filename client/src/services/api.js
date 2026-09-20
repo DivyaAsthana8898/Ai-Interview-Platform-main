398fcfd055f222187dfacc4006ab0f224a2bbaf1
Divya Asthana <divyakumar12a48@gmail.com>
2026-09-20 16:54:51 +0530

    Update API baseURL to include '/api' endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-interview-platform-main-mx4y.vercel.app",
+  baseURL: "https://ai-interview-platform-main-mx4y.vercel.app/api",
 });
 
 export default API;
